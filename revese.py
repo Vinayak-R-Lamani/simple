29953a303db2057cf49b43764db6ee10c403aaab
Vinayak-R-Lamani <rajulamani9888@gmail.com>
2025-02-09 12:38:39 +0530

    reverse the array

---
+++ revese.py
@@ -0,0 +1,10 @@
+def reverse_array(arr:list):
+    i  = 0 
+    j = len(arr) - 1
+    while i<j:
+        arr[i] , arr[j] = arr[j] , arr[i]
+        i += 1
+        j -= 1
+    return arr
+arr = [1,2,3,4,5] 
+print(reverse_array(arr))
